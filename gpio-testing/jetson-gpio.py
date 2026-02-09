f9f4d92aded8b54f35d8598e7d554480b3cf6b55
AJ12furbish <aj2004.ab@gmail.com>
2026-02-09 14:54:48 -0600

    modify gpio testing scripts

---
+++ gpio-testing/jetson-gpio.py
@@ -0,0 +1,21 @@
+import Jetson.GPIO as GPIO
+import time
+
+PIN = 15  # physical pin number
+
+GPIO.setmode(GPIO.BOARD)   # use physical pin numbering
+GPIO.setup(PIN, GPIO.OUT, initial=GPIO.LOW)
+
+try:
+    while True:
+        GPIO.output(PIN, GPIO.HIGH)
+        time.sleep(0.5)
+        GPIO.output(PIN, GPIO.LOW)
+        time.sleep(0.5)
+
+except KeyboardInterrupt:
+    pass
+
+finally:
+    GPIO.cleanup()
+
